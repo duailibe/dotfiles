2e7ec7fb6c304c620ae9435d584f0015f8b2640d
Lucas Duailibe <lucasds@gmail.com>
2021-11-21 17:51:33 -0300

    improves fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,5 @@
 set -x -g LC_ALL en_US.UTF-8
 set -x -g LANG en_US.UTF-8
 
-source ~/.dotfiles/fish/path.fish
-
 source (brew --prefix)/etc/grc.fish
 source (brew --prefix asdf)/libexec/asdf.fish
--- fish/setup.sh
+++ fish/setup.sh
@@ -6,13 +6,28 @@ fish="$HOME/.config/fish"
 
 source "$dotfiles/script/helpers.sh"
 
-symlink "$here/config.fish" "$fish/config.fish"
+cd "$here"
 
-for dir in "conf.d" functions completions; do
-  if [ -e "$here/$dir" ]; then
-    for file in "$here/$dir"/*; do
-      filename=$(basename "$file")
-      symlink "$file" "$fish/$dir/$filename"
-    done
+for file in $(find * -type f -not -wholename "setup.sh"); do
+  dir=$(dirname "$file")
+  if [ ! -e "$fish/$dir" ]; then
+    mkdir -p "$fish/$dir"
   fi
+  symlink "$here/$file" "$fish/$file"
+done
+
+exit 0
+
+for dir in "." "conf.d" functions completions; do
+  if [ ! -e "$here/$dir" ]; then
+    continue
+  fi
+
+  for file in "$here/$dir"/*; do
+    if [ "$file" == "${BASH_SOURCE[0]}" ]; then
+      continue
+    fi
+    filename=$(basename "$file")
+    echo symlink "$file" "$fish/$dir/$filename"
+  done
 done
