5997e375b6227ef4ac44ca3a39854649cc22fab8
Lucas Duailibe <lucasds@gmail.com>
2021-12-02 17:35:13 -0300

    improve homebrew step
    - update Brewfile
    - update brew install command
    - brew bundle doesn't need to upgrade, we call `brew upgrade` right after

--- homebrew/setup.sh
+++ homebrew/setup.sh
@@ -5,14 +5,15 @@ here="$dotfiles/homebrew"
 source "$dotfiles/script/helpers.sh"
 
 if ! which brew &> /dev/null; then
-  info "Installing Homebrew"
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  success "Done"
+  _task() {
+    printf "\n" | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  }
+  task "Installing Homebrew" _task
 fi
 
 _task() {
   runcmd "brew update"
-  runcmd "brew bundle --no-lock --file $here/Brewfile"
+  runcmd "brew bundle --no-lock --no-upgrade --file $here/Brewfile"
   runcmd "brew upgrade"
   runcmd "brew cleanup"
 }
