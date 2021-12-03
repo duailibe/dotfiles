646c2096a59f52f1f3994e6d4cfd6563159bc0da
Lucas Duailibe <lucasds@gmail.com>
2021-12-02 21:57:44 -0300

    fish setup: automatically call chsh

--- fish/setup.sh
+++ fish/setup.sh
@@ -6,6 +6,14 @@ fish="$HOME/.config/fish"
 
 source "$dotfiles/script/helpers.sh"
 
+if ! fgrep -q "$(brew --prefix)/bin/fish" /etc/shells; then
+  _chsh() {
+    echo "$(brew --prefix)/bin/fish" | sudo tee -a /etc/shells;
+    chsh -s "$(brew --prefix)/bin/fish"
+  }
+  task "Set fish as default shell"
+fi
+
 find "$here" -type f -not -wholename "$here/setup.sh" | while read -r file; do
   dest="$fish/${file#"$here/"}"
   if [ ! -e "$(dirname "$dest")" ]; then
