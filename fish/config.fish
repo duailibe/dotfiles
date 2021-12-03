01492b4e4c97bb53511d7898432af9c4a0bdb2ed
Lucas Duailibe <lucasds@gmail.com>
2021-12-02 21:57:24 -0300

    fish: fix path for homebrew in m1

--- fish/config.fish
+++ fish/config.fish
@@ -3,14 +3,17 @@ set -x -g LANG en_US.UTF-8
 set -x -g EDITOR vim
 
 # Path configuration
-set default_paths /usr/bin /usr/sbin /bin /sbin
-set homebrew_paths /usr/local/bin /usr/local/sbin
+set default_paths /usr/local/bin /usr/local/sbin /usr/bin /usr/sbin /bin /sbin
+set homebrew_path
+if test (uname -m) = "arm64"
+  set homebrew_path /opt/homebrew/bin
+end
 set local_paths "$HOME/.dotfiles/bin" "$HOME/.local/bin"
 
 # android tools
 set android_path "$HOME/Library/Android/sdk/tools"
 
-set -gx PATH $local_paths $homebrew_paths $default_paths $android_path
+set -gx PATH $local_paths $homebrew_path $default_paths $android_path
 
 # Homebrew installed
 source (brew --prefix)/etc/grc.fish
