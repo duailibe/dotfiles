20b647a643c9dd103012a1a32390a3cb9ca63f11
Lucas Duailibe <lucasds@gmail.com>
2025-07-31 17:04:53 -0300

    enable guess-indent

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -171,15 +171,12 @@ rtp:prepend(lazypath)
 require('lazy').setup({
   -- [[ Plugin Specs list ]]
 
-  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
-
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
   --
   -- Use `opts = {}` to automatically pass options to a plugin's `setup()` function, forcing the plugin to be loaded.
-  --
+  { 'NMAC427/guess-indent.nvim', opts = {} }, -- Detect tabstop and shiftwidth automatically
 
   -- Alternatively, use `config = function() ... end` for full control over the configuration.
   -- If you prefer to call `setup` explicitly, use:
